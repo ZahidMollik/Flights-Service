13936f6d9211d531beca42ab68d16afd1a962fc4
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2023-12-26 13:30:42 +0600

    added airplane model

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,10 +1,8 @@
 const {StatusCodes}=require('http-status-codes');
 const {SuccessResponse}=require('../utils/common');
-const info=function (req,res){
+ function info (req,res){
   return res.status(StatusCodes.OK)
             .json(SuccessResponse);
 }
 
-module.exports={
-  info
-};
+module.exports=info;
